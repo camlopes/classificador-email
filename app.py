217918e24459c5ea835fc393dae725504f5283b2
Camila Lopes <63123059+camlopes@users.noreply.github.com>
2025-09-30 18:03:06 -0300

    Processamento do email usando técnicas de pré-processamento de NLP

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, request, render_template, jsonify
 import PyPDF2
 import google.generativeai as genai
 import os
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import RSLPStemmer
+from nltk.tokenize import word_tokenize
 
 app = Flask(__name__)
 
@@ -13,8 +17,6 @@ if not api_key:
     raise ValueError("GEMINI_API_KEY não encontrada nas variáveis de ambiente.")
 genai.configure(api_key=api_key)
 
-model = genai.GenerativeModel('gemini-2.5-flash')
-
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -23,7 +25,22 @@ def home():
 def contact():
     return render_template("contact.html")
 
-def prompt_ia(email_conteudo):
+def pre_processamento_email(email_conteudo):
+    email_conteudo_minusculas = email_conteudo.lower()
+    
+    email_tokens = word_tokenize(email_conteudo_minusculas, language='portuguese')
+    
+    stop_words = set(stopwords.words("portuguese"))
+    email_tokens_sem_stopwords = [t for t in email_tokens if t.isalpha() and t not in stop_words]
+    
+    stemmer = RSLPStemmer()
+    pre_processado_email = [stemmer.stem(t) for t in email_tokens_sem_stopwords]
+
+    return " ".join(pre_processado_email)
+
+def prompt_ia(email_pre_processado, email_original):
+    model = genai.GenerativeModel('gemini-2.5-flash')
+    
     prompt_categoria = """
         De acordo com as categorias de emails abaixo: 
         Produtivo: Emails que requerem uma ação ou resposta específica (ex.: solicitações de suporte técnico, atualização sobre casos em aberto, dúvidas sobre o sistema).
@@ -32,14 +49,14 @@ def prompt_ia(email_conteudo):
         
         Atenção: Responda somente com a palavra "Produtivo" ou "Improdutivo".
         
-        """ + email_conteudo
+        """ + email_pre_processado
 
     prompt_resposta = """
-        Responda o email abaixo exatamente com o inicio e final mostrados:
+        Responda o "Email pre processado" abaixo exatamente com o inicio e final mostrados:
         
         Inicio padrao da resposta do email:
-        Assunto: Re: [assunto do email abaixo]
-        Para: [email da pessoa que enviou o email abaixo]
+        Assunto: Re: [assunto que esta informado em "Email original" abaixo]
+        Para: [email da pessoa que enviou o email. Essa informacao esta em "Email original"]
         
         Final padrao da resposta do email:
         Atenciosamente,
@@ -47,14 +64,18 @@ def prompt_ia(email_conteudo):
         Suporte
         empresaTM
 
-        """ + email_conteudo
+        Email original:
+        """ + email_original + """
+        
+        Email pre processado:
+        """ + email_pre_processado
 
     try:
         categoria = model.generate_content(prompt_categoria).text.strip()
 
         if categoria == "Improdutivo":
             prompt_resposta += """ 
-            Responda com uma mensagem automatica de acordo com o assunto do email """
+            Responda com uma mensagem automatica de acordo com o assunto do email que esta em "Email original" """
         
         resposta_sugerida = model.generate_content(prompt_resposta).text.strip()  
         return categoria, resposta_sugerida
@@ -92,7 +113,9 @@ def upload_file():
     if email_conteudo == "":
         return jsonify({"erro": "Nenhum conteúdo foi enviado."}), 400
     
-    categoria, resposta_sugerida = prompt_ia(email_conteudo)
+    email_pre_processado = pre_processamento_email(email_conteudo)
+
+    categoria, resposta_sugerida = prompt_ia(email_pre_processado, email_conteudo)
     if categoria == None and resposta_sugerida == None:
         return jsonify({"erro": "Erro ao chamar a Gemini API"}), 400
 
